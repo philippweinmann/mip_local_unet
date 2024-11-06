ae2d6285e49668dcfb18d89b36b4bdef59bbfece
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-11-06 22:11:53 +0100

    unet there, no training yet

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # %%
+%load_ext autoreload
+%autoreload 2
+
 from generate import get_image
+from unet import UNet
 import numpy as np
 from matplotlib import pyplot as plt
-
+import torch
 # %%
 image, mask = get_image()
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
@@ -11,4 +15,14 @@ ax[0].set_title('Image')
 
 ax[1].imshow(mask, cmap='gray')
 ax[1].set_title('Mask')
+# %%
+print(image.shape)
+image = image[None, None, :, :]
+print(image.shape)
+
+# %%
+t_image = torch.tensor(image).float()
+model = UNet(in_channels=1, num_classes=1)
+# %%
+model(t_image)
 # %%
