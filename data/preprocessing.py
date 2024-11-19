57351d41f99c90763ec757e84d318b32d04a75ff
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-11-19 20:52:51 +0100

    added normalization to preprocessing

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,6 +2,11 @@ from data_generation.config import mean_voxel_intensity
 from data.data_utils import shift_mean, clip_scans
 
 def preprocess_ccta_scan(image):
+    min_value = -500
+    max_value = 1000
     # image = shift_mean(image, mean_voxel_intensity)
-    image = clip_scans(image, -500, 1000)
+    image = clip_scans(image, min_value, max_value)
+
+    # normalize the image from 0 to 1
+    image = (image - min_value) / (max_value - min_value)
     return image
