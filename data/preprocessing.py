44891ae66d49ebf90894e948cf9da2d844a989e7
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-11-19 18:23:13 +0100

    added preprocessing file

---
+++ data/preprocessing.py
@@ -0,0 +1,7 @@
+from data.constants import mean_voxel_intensity
+from data.data_utils import shift_mean
+
+def preprocess_ccta_scan(image):
+    image = shift_mean(image, mean_voxel_intensity)
+
+    return image
