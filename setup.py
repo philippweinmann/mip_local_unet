b347e7a47b7f58ee6c052921369e6f8c04742f79
philipp weinmann <philipp.weinmann71@gmail.com>
2024-12-01 20:35:07 +0100

    fixed first test, added setup.py file to project

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='mip_unet',
+      version='0.1',
+      description='My model for the MIP project',
+      author='Philipp Weinmann',
+      author_email='philipp.weinmann71@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+        # Dependencies go here
+        'numpy', 
+        "matplotlib==3.9.2", 
+        "scikit-learn==1.5.2", 
+        "scikit-image", 
+        "torch==2.5.0", 
+        "opencv_python_headless==4.10.0.84", 
+        "scipy==1.14.1",
+        "nibabel",
+    ],
+     )
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,23 @@
 import unittest
+from models.unet3D import UNet3D
+from models.net_utils import get_best_device, prepare_image_for_network_input, prepare_image_for_analysis
+import torch
+import numpy as np
 
-class TestStringMethods(unittest.TestCase):
+class TestModelFunctions(unittest.TestCase):
+    def setUp(self):
+        self.model = UNet3D(in_channels=1, num_classes=1)
+        self.device = get_best_device()
+        self.model.to(self.device)
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        patch_size = 128
+        self.empty_input = prepare_image_for_network_input(np.zeros((patch_size, patch_size, patch_size))).to(self.device)
+        self.full_input = prepare_image_for_network_input(np.ones((patch_size, patch_size, patch_size))).to(self.device)
+        return super().setUp()
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_model(self):
+        pred_empty = self.model(self.empty_input)
+        pred_full = self.model(self.full_input)
 
 if __name__ == '__main__':
     unittest.main()
