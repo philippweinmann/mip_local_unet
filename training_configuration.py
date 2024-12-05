3b347e0da0db5ebae800088f4509bc4320325365
philippweinmann <philipp.weinmann71@gmail.com>
2024-12-05 10:14:48 +0000

    added training logs folder

--- training_configuration.py
+++ training_configuration.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 PATCH_SIZE = 128
 PATCHES_FOLDER = Path("/home/tu-philw/group/gecko/pweinmann/mip_local_unet/preprocessed_patches/")
+TRAINING_LOGS_FOLDER = Path("/home/tu-philw/group/gecko/pweinmann/mip_local_unet/training_logs/")
 
 AMT_VAL_PATIENTS = 20
 AMT_TEST_PATIENTS = 40
