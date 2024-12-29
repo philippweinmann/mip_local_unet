a0a1a23ee22e9c9ed06722fae4720ddfbb7e2732
Philipp Weinmann <philipp.weinmann71@gmail.com>
2024-12-29 17:07:06 +0100

    fixed potential bug

--- models/net_visualizations.py
+++ models/net_visualizations.py
@@ -2,10 +2,13 @@ from data.visualizations import visualize_3d_matrices
 from models.net_utils import prepare_image_for_network_input, prepare_image_for_analysis
 
 def three_d_visualize_model_progress(model, get_image_fct):
+    # not tested
     original_img, original_mask = get_image_fct()
 
     pred = model(prepare_image_for_network_input(original_img))
 
-    visualize_3d_matrices(original_img, original_mask, prepare_image_for_analysis(pred))
+    matrices = [original_img, original_mask, prepare_image_for_analysis(pred)]
+    titles = ["Original Image", "Original Mask", "Prediction"]
+    visualize_3d_matrices(matrices, titles, global_title="Model Progress")
 
     return original_mask, prepare_image_for_analysis(pred)
