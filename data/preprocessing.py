b100ec57407b35456ddec61a4ce6effc854ce407
philippweinmann <philipp.weinmann71@gmail.com>
2025-01-02 15:33:45 +0000

    visualization of preprocessing without patching is now done

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -23,27 +23,38 @@ def center_image(image):
     
     return image
 
-def resample_image(image, original_spacing, target_spacing):
+def resample_image(image, original_spacing, target_spacing, is_label):
     
     zoom_factors = [original_spacing[i] / target_spacing[i] for i in range(3)]
-    resampled_image = zoom(image, zoom_factors, order=1)  # Linear interpolation
+    
+    order = 0 if is_label else 1
+    resampled_image = zoom(image, zoom_factors, order=order)  # Linear interpolation
     
     return resampled_image
 
 def preprocess_ccta_scan(image, mask, original_voxel_spacing):
+    '''
+    Steps:
+        1. clipping
+        2. min/max normalization
+        3. centering
+        4. fix voxel spacings
+    '''
+    # these two values define where we clip
     min_value = -600
     max_value = 1000
-    # image = shift_mean(image, mean_voxel_intensity)
+    
+    # 1. clipping
     image = clip_scans(image, min_value, max_value)
 
-    # normalize the image from 0 to 1
+    # 2. min/max normalization
     image = min_max_normalization(image, min_value, max_value)
     
-    # center the image
+    # 3. centering
     image = center_image(image)
     
-    # fix the voxel spacing
-    image = resample_image(image, original_voxel_spacing, target_spacing = [0.3, 0.3, 0.3])
-    mask = resample_image(mask, original_voxel_spacing, target_spacing = [0.3, 0.3, 0.3])
+    # 4. fix voxel spacing
+    image = resample_image(image, original_voxel_spacing, target_spacing = [0.3, 0.3, 0.3], is_label=False)
+    mask = resample_image(mask, original_voxel_spacing, target_spacing = [0.3, 0.3, 0.3], is_label=True)
     
     return image, mask
